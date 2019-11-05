dd20a67fc6561d8f6e57a3b78b3a9dda8bf6347e
KevinM818 <Kmangal818@gmail.com>
2019-11-05 00:00:15 -0500

    Added update DB on install

--- server.js
+++ server.js
@@ -1,22 +1,59 @@
-const dotenv = require('dotenv');
+require('isomorphic-fetch');
+require('dotenv').config();
+const mongoose = require('mongoose');
 const Koa = require('koa');
-const mongoose = require('koa-mongoose');
+const session = require('koa-session');
+const shopifyAuth = require('@shopify/koa-shopify-auth');
+const Shop = require('./models/shop');
+
+mongoose.connect(process.env.MONGODB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
 
 const port = process.env.PORT || 3000;
 const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
 const app = new Koa();
 
-app.use(mongoose({
-	uri: 'mongodb+srv://KevinM:330.mongo@cluster0-vdm6j.mongodb.net/test?retryWrites=true&w=majority',
-	mongodbOptions: {
-		poolSize: 5,
-		native_parser: true
-	},
-	events: {
-		connected() {console.log('connected');}
-	}
-}));
+app.keys = [SHOPIFY_API_SECRET_KEY];
 
-
+app
+	.use(session(app))
+	.use(
+		shopifyAuth.default({
+			apiKey: SHOPIFY_API_KEY,
+			secret: SHOPIFY_API_SECRET_KEY,
+			scopes: ['read_products', 'write_products'],
+			async afterAuth(ctx) {
+				const { shop, accessToken } = ctx.session;
+				try {
+					const store = new Shop({shop, accessToken});
+					await store.save();
+					console.log('Ran save');
+					ctx.redirect('/')
+				} catch(e) {console.log('Error saving shop', e);}
+			}
+		})
+	)
+	.use(shopifyAuth.verifyRequest())
+	.use(async ctx => {
+	  ctx.body = 'Hello World';
+	});
 
 app.listen(port, () => console.log(`Running on PORT: ${port}`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
